5c1d1254e9eb5f92bb68910984c3a495b3535507
Luis Edward Miranda <luisedward.miranda@gmail.com>
2025-05-24 17:59:48 +0800

    chore: add minification feature

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,32 @@
-module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("src/assets");
-    eleventyConfig.addPassthroughCopy("src/favicon");
-    eleventyConfig.addPassthroughCopy("CNAME");
+const htmlmin = require("html-minifier-terser");
 
-    return {
-        dir: {
-            input: "src",
-            includes: "_includes",
-            data: "_data",
-            output: "_site"
-        }
-    };
+module.exports = function (eleventyConfig) {
+  // Passthrough copy for assets, favicon, and CNAME
+  eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("src/favicon");
+  eleventyConfig.addPassthroughCopy("CNAME");
+
+  // HTML minification transform
+  eleventyConfig.addTransform("htmlmin", async function (content, outputPath) {
+    if (outputPath && outputPath.endsWith(".html")) {
+      return await htmlmin.minify(content, {
+        removeComments: true,
+        collapseWhitespace: true,
+        useShortDoctype: true,
+        minifyCSS: true,
+        minifyJS: true,
+      });
+    }
+    return content;
+  });
+
+  // Directory config
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      data: "_data",
+      output: "_site",
+    },
+  };
 };
